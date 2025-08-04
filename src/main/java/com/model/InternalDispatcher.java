d6d56a4a2287d68c8c3b1dd27ed39f41966d4625
Eomaxl <souravsnigdh@gmail.com>
2025-08-04 12:40:10 +0530

    Implemented elevator controller class

---
+++ src/main/java/com/model/InternalDispatcher.java
@@ -0,0 +1,10 @@
+package com.model;
+
+import java.util.List;
+
+public class InternalDispatcher {
+
+    List<ElevatorController> elevatorControllerList = ElevatorCreator.elevatorControllerList;
+
+    public void submitInternalRequest(int floor, ElevatorCar elevatorCar) {}
+}
